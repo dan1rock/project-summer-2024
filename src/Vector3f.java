b4828c7ceef53786e58b428785fe8d3a81618e7a
Danylo Holets <90461399+dan1rock@users.noreply.github.com>
2024-05-26 01:24:47 +0300

    Improved operations with Vector3f

--- src/Vector3f.java
+++ src/Vector3f.java
@@ -3,25 +3,54 @@ package src;
 public class Vector3f {
     public float x, y, z;
 
+    public Vector3f() {
+        this.x = 0f;
+        this.y = 0f;
+        this.z = 0f;
+    }
+
+
     public Vector3f(float x, float y, float z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public void normalize() {
+    public Vector3f(Vector3f vector3f) {
+        this.x = vector3f.x;
+        this.y = vector3f.y;
+        this.z = vector3f.z;
+    }
+
+    public Vector3f normalize() {
         float length = (float) Math.sqrt(x * x + y * y + z * z);
         if (length != 0) {
             x /= length;
             y /= length;
             z /= length;
         }
+        return this;
     }
 
-    public void mul(float value) {
+    public Vector3f mul(float value) {
         x *= value;
         y *= value;
         z *= value;
+        return this;
+    }
+
+    public Vector3f add(Vector3f vector3f) {
+        x += vector3f.x;
+        y += vector3f.y;
+        z += vector3f.z;
+        return this;
+    }
+
+    public Vector3f sub(Vector3f vector3f) {
+        x -= vector3f.x;
+        y -= vector3f.y;
+        z -= vector3f.z;
+        return this;
     }
 
     public static Vector3f sub(Vector3f left, Vector3f right) {
